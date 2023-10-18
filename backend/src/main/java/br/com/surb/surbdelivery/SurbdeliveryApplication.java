7105540065a1b807278c60cc3456994b86a6dc74
claudiobelempb <claudio.c.lima@hotmail.com>
2023-10-18 08:47:34 -0300

    first commit

---
+++ backend/src/main/java/br/com/surb/surbdelivery/SurbdeliveryApplication.java
@@ -0,0 +1,13 @@
+package br.com.surb.surbdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurbdeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurbdeliveryApplication.class, args);
+	}
+
+}
